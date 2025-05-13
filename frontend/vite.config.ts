def219aab3e14fb7c3a2e22302a33830c92af36f
Enk0rt <delovoyartem4ick@gmail.com>
2025-05-13 16:19:20 +0300

    feat: updated readme file, updated explanation how to run a build, updated parameter to env-template

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server:{
-    port: 3000
-  }
-})
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, resolve(__dirname, '..'), '');
+
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_FRONTEND_PORT)||5173,
+    }
+  };
+});
