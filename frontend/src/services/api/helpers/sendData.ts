19f7ca2f58619a2bcc8e43f8be3219b5f452213e
Enk0rt <delovoyartem4ick@gmail.com>
2025-05-16 13:48:08 +0300

    fix: change console log to logger

--- frontend/src/services/api/helpers/sendData.ts
+++ frontend/src/services/api/helpers/sendData.ts
@@ -1,25 +1,27 @@
 import type {IMeal} from "../../../models/meal/IMeal.ts";
 import type {IEntryFormData} from "../../../models/entry-form-data/IEntryFormData.ts";
 import {axiosInstance} from "../apiOperations.ts";
+import log from "loglevel";
 
 type AllowedAxiosMethods = 'post' | 'put' | 'patch';
 
-export const sendData = async ({ name, energy_kcal, consumed_amount }: IEntryFormData,method:AllowedAxiosMethods,url:string):Promise<IMeal | undefined> => {
-        const kcal = Number(energy_kcal)
-        const weight = Number(consumed_amount)
+export const sendData = async ({name, energy_kcal, consumed_amount}: IEntryFormData, method: AllowedAxiosMethods, url: string): Promise<IMeal | undefined> => {
+    const kcal = Number(energy_kcal)
+    const weight = Number(consumed_amount)
 
-        if(isNaN(kcal)){
-            console.log(`Incorrect value kcal: ${kcal}`)
-            return undefined
-        }
-        if(isNaN(weight)){
-            console.log(`Incorrect value weight: ${weight}`)
-            return undefined
-        }
+    if (isNaN(kcal)) {
+        log.error(`Incorrect value kcal: ${kcal}`)
+        return undefined
+    }
+    if (isNaN(weight)) {
+        log.error(`Incorrect value weight: ${weight}`)
+        return undefined
+    }
 
-        const response = await axiosInstance[method]<IMeal>(url, {
-            name,
-            energy_kcal: kcal ,
-            consumed_amount: weight})
-        return response.data
+    const response = await axiosInstance[method]<IMeal>(url, {
+        name,
+        energy_kcal: kcal,
+        consumed_amount: weight
+    })
+    return response.data
 }
