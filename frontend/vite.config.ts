120c8eea4775882d8a3c64e2dcf67f225717c491
LonerLykos <loner.lykos.project@gmail.com>
2025-05-16 14:33:14 +0300

    feat: front commit

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,10 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { defineConfig} from 'vite';
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, resolve(__dirname, '..'), '');
-
-  return {
-    plugins: [react()],
-    server: {
-      port: Number(env.VITE_FRONTEND_PORT)||5173,
-    },
-    define: {
-      'import.meta.env.VITE_FRONTEND_PORT': JSON.stringify(env.VITE_FRONTEND_PORT),
-      'import.meta.env.VITE_BACKEND_SERVER_PORT': JSON.stringify(env.VITE_BACKEND_SERVER_PORT)
-    },
-  };
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    outDir: '../client',
+    emptyOutDir: true
+  }
 });
